ffbe120f9236e393a4487963c1ed79829a3cd785
Harish G~ <2021wb86904@wilp.bits-pilani.ac.in>
2026-09-19 01:47:38 +0530

    CI Test

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("BITS DevOps Continuous Integration Successful - 2021WB86904");
     }
 }
